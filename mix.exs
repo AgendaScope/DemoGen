fb811d0f1b48a92029d385b8f430dfe64a4787d9
Matt Mower <matt@agendascope.com>
2025-02-23 01:05:46 +0000

    v0.1.7 Getting ready for hex.pm

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule DemoGen.MixProject do
       app: :demo_gen,
       name: "DemoGen",
       description: description(),
-      version: "0.1.6",
+      version: "0.1.7",
       elixir: "~> 1.17",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
